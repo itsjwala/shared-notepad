0af7c23b93523b9f86cd385ada72c3a0625604a6
jigarWala <jigar.wala@somaiya.edu>
2017-05-03 19:25:52 +0530

    modified renderer

--- public/renderer.js
+++ public/renderer.js
@@ -1,10 +1,8 @@
-var socket;
+var socket=null;
 var notepad=document.querySelector('textarea');
   
-// socket=io.connect('http://localhost:3000');
-
-socket=io.connect('https://calm-plains-77269.herokuapp.com:'+location.port);
-
+ socket=io.connect(location.href);
+ 
 notepad.onkeyup=function(){
   //give text to server
   socket.emit('text',{gem:this.value});
@@ -12,5 +10,5 @@ notepad.onkeyup=function(){
 
 //on receiving data from server due to text event
  socket.on('text',(data)=>{
-notepad.value=data.gem;a
+notepad.value=data.gem;
  });
